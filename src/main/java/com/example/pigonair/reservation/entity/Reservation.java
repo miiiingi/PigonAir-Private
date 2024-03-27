754025608ac1b3627903b05f8f10310d273c121b
gbk1414 <gbk1414@gmail.com>
2024-03-27 15:54:14 +0900

    <refactor>: application.properties 삭제

--- src/main/java/com/example/pigonair/reservation/entity/Reservation.java
+++ src/main/java/com/example/pigonair/reservation/entity/Reservation.java
@@ -19,7 +19,7 @@ public class Reservation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@OneToOne // 추후에 @ManyToOne
 	private Member member;
 
 	@ManyToOne
