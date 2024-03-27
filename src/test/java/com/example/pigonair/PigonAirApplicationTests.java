b361526984083ee3236024e3525c8e6e8298df7b
gbk1414 <gbk1414@gmail.com>
2024-03-27 10:56:00 +0900

    feat: 프로젝트 뼈대 생성

---
+++ src/test/java/com/example/pigonair/PigonAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pigonair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigonAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
