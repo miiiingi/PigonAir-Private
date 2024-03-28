303f51535c73878e038fb7ec3df4f732e78637f8
Mingi Kim <71509516+miiiingi@users.noreply.github.com>
2024-03-28 15:13:30 +0900

    Feature/1 (#7)
    * chore: jwt, swagger, lombok 빌드 추가
    
    * feat: 로그인, 회원가입 페이지추가(spring security 적용)
    
    * feat: 로그인 필터가 적용되는 기본 페이지 추가 및 페이지 연결
    
    * fix: 로그인한 상태에서 페이지 이동 하는 것과 로그인하지 않은 상태에서 페이지 이동하는 것 수정
    
    * feat: 로그아웃 기능 추가
    
    * refactor: 불필요한 로그 삭제
    
    * feat: 회원가입 페이지에 홈으로 이동하는 버튼 추가
    
    * refactor: 패키지 구조 변경
    
    * fix: swagger config 이름 변경
    
    * refactor: 패키지 구조 변경에 따른 경로 변경
    
    * fix: memberservice interface로 변경
    
    * fix: swagger에서 로그인 사용할 수 있도록 수정
    
    * refactor: 불필요한 코드 삭제
    
    * feat: 회원가입, 로그인 테스트 코드 작성
    
    * fix: 로그인 실패 시 에러 처리 추가
    
    * fix: no args access level 추가

--- src/test/java/com/example/pigonair/PigonAirApplicationTests.java
+++ src/test/java/com/example/pigonair/PigonAirApplicationTests.java
@@ -1,13 +1,77 @@
 package com.example.pigonair;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.example.pigonair.domain.member.dto.MemberRequestDto;
+import com.example.pigonair.domain.member.repository.MemberRepository;
+import com.example.pigonair.domain.member.service.MemberServiceImpl;
+import com.example.pigonair.global.config.common.exception.CustomException;
+import com.example.pigonair.global.config.common.exception.ErrorCode;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
+@Transactional
 class PigonAirApplicationTests {
+	@Autowired
+	private MemberServiceImpl memberService;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void signUpTest() {
+		// given
+		MemberRequestDto.SignupRequestDto requestDto = new MemberRequestDto.SignupRequestDto(
+			"test96@test.com", "1234", "test", "010-0000-0000"
+		);
+		if (memberRepository.findByEmail(requestDto.email()).isPresent()) {
+			assertThrows(CustomException.class, () -> {
+				memberService.signUp(requestDto);
+			});
+
+		} else {
+			assertDoesNotThrow(() -> {
+				memberService.signUp(requestDto);
+			});
+		}
+
+	}
+
+	@Test
+	void loginTest() {
+		// given
+		MemberRequestDto.LoginRequestDto requestDto = new MemberRequestDto.LoginRequestDto(
+			"test01@test.com", "1234"
+		);
+
+		Authentication authentication = authenticationManager.authenticate(
+			new UsernamePasswordAuthenticationToken(
+				requestDto.email(),
+				requestDto.password(),
+				null
+			)
+		);
+		if (authentication.isAuthenticated()) {
+			// 로그인 성공
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} else {
+			// 로그인 실패 - CustomException 발생
+			throw new CustomException(ErrorCode.INVALID_EMAIL_OR_PASSWORD);
+		}
+
+	}
 
 }
