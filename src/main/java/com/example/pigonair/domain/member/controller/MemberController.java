5117cffb8bad52cc4dd5890e48762d5f9258de0d
miiiingi <kimmingi722@gmail.com>
2024-03-28 11:31:11 +0900

    fix: swagger에서 로그인 사용할 수 있도록 수정

--- src/main/java/com/example/pigonair/domain/member/controller/MemberController.java
+++ src/main/java/com/example/pigonair/domain/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.pigonair.domain.member.service.MemberServiceImpl;
 import com.example.pigonair.global.config.common.exception.CustomException;
@@ -66,7 +67,7 @@ public class MemberController {
 
 	@Operation(summary = "로그인", description = "로그인 시 필요한 정보를 입력합니다.")
 	@PostMapping("/login")
-	public void login(MemberRequestDto.LoginRequestDto requestDto) {
+	public void login(@RequestBody MemberRequestDto.LoginRequestDto requestDto) {
 
 	}
 
