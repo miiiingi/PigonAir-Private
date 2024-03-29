890a745e2befa25e988b79931eb63c143c44c857
miiiingi <kimmingi722@gmail.com>
2024-03-29 18:08:46 +0900

    chore: swagger 빌드 추가

--- src/main/java/com/example/pigonair/PigonAirApplication.java
+++ src/main/java/com/example/pigonair/PigonAirApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://52.78.141.125.nip.io", description = "hh99finalproject")})
 @SpringBootApplication
 public class PigonAirApplication {
 	public static void main(String[] args) {
