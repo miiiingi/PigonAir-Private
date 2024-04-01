486170b97f5153514a9bcce72d5cf60bdb84796f
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-04-01 10:30:33 +0900

    Feature/3/suhyun (#26)
    * fix(): Member, Reservation 연관관계 ManyToOne, Seat Reservation연관관계 OneToOne으로 변경
    
    * feat(): 1분(추후에 15분으로 변경)안에 결제가 되지 않을 시 예약좌석 lock 풀기 및 예약 삭제

--- src/main/java/com/example/pigonair/PigonAirApplication.java
+++ src/main/java/com/example/pigonair/PigonAirApplication.java
@@ -2,6 +2,7 @@ package com.example.pigonair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @EnableJpaAuditing
 @OpenAPIDefinition(servers = {@Server(url = "https://52.78.141.125.nip.io", description = "hh99finalproject")})
 @SpringBootApplication
+@EnableScheduling
 public class PigonAirApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PigonAirApplication.class, args);
