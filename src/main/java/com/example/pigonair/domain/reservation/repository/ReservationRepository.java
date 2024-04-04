45deca168637f0d6e26cb2b90fb55b6b843b1597
uha9218 <uha0542@naver.com>
2024-04-05 07:55:55 +0900

    refactor: 티켓 리스트 형식으로 출력, 결제 실패시 /ticket으로 이동

--- src/main/java/com/example/pigonair/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/pigonair/domain/reservation/repository/ReservationRepository.java
@@ -1,19 +1,20 @@
 package com.example.pigonair.domain.reservation.repository;
 
-import com.example.pigonair.domain.member.entity.Member;
-import com.example.pigonair.domain.reservation.entity.Reservation;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import com.example.pigonair.domain.member.entity.Member;
+import com.example.pigonair.domain.reservation.entity.Reservation;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findByMemberAndIsPayment(Member member, Boolean isPayment);
-	Optional<Reservation> findByMemberId(Long memberId);
+	List<Reservation> findByMemberAndIsPayment(Member member, Boolean isPayment);
 
-    List<Reservation> findByIsPaymentFalseAndReservationDateBefore(LocalDateTime cutoffDate);
+	List<Reservation> findAllByMemberId(Long memberId);
+
+	List<Reservation> findByIsPaymentFalseAndReservationDateBefore(LocalDateTime cutoffDate);
 
 }
