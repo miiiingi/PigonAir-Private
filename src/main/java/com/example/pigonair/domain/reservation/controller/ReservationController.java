06f280951b515cd99fe62b3bbde2228a4e860616
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-04-04 16:05:39 +0900

    Feature/3/suhyun (#32)
    * fix(): Member, Reservation 연관관계 ManyToOne, Seat Reservation연관관계 OneToOne으로 변경
    
    * feat(): 1분(추후에 15분으로 변경)안에 결제가 되지 않을 시 예약좌석 lock 풀기 및 예약 삭제
    
    * refactor(): custoemException 적용
    
    * Update ErrorCode.java
    
    ---------
    
    Co-authored-by: LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>

--- src/main/java/com/example/pigonair/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/example/pigonair/domain/reservation/controller/ReservationController.java
@@ -1,6 +1,13 @@
 package com.example.pigonair.domain.reservation.controller;
 
-import java.util.List;
+
+import com.example.pigonair.domain.reservation.dto.ReservationRequestDto;
+import com.example.pigonair.domain.reservation.dto.ReservationResponseDto;
+import com.example.pigonair.domain.reservation.service.ReservationService;
+import com.example.pigonair.global.config.common.exception.CustomException;
+import com.example.pigonair.global.config.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +18,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.example.pigonair.domain.reservation.dto.ReservationRequestDto;
-import com.example.pigonair.domain.reservation.dto.ReservationResponseDto;
-import com.example.pigonair.domain.reservation.service.ReservationService;
-import com.example.pigonair.global.config.security.UserDetailsImpl;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ReservationController {
-	private final ReservationService reservationService;
 
-	@PostMapping("/api/reservation") // 예약 진행
-	public ResponseEntity<?> saveReservation(@RequestBody ReservationRequestDto requestDto,
-		@AuthenticationPrincipal UserDetailsImpl userDetails) {  // 로그인 기능 구현 완료 시 UserDetail 추가
-		try {
-			reservationService.saveReservation(requestDto, userDetails);
-			return ResponseEntity.ok().build();
-		} catch (NullPointerException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		} catch (IllegalArgumentException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).build();
-		}
-	}
+    private final ReservationService reservationService;
 
-	@GetMapping("/api/reservation") // 예약 진행
-	public String getReservations(@AuthenticationPrincipal UserDetailsImpl userDetails,
-		Model model) {  // 로그인 기능 구현 완료 시 UserDetail 추가
-		List<ReservationResponseDto> reservations = reservationService.getReservations(userDetails);
-		model.addAttribute("reservations", reservations);
-		return "/reservation/reservation_history";
-	}
+
+    @PostMapping("/api/reservation") // 예약 진행
+    public ResponseEntity<?> saveReservation(@RequestBody ReservationRequestDto requestDto,
+                                             @AuthenticationPrincipal UserDetailsImpl userDetails) {  // 로그인 기능 구현 완료 시 UserDetail 추가
+        try {
+            reservationService.saveReservation(requestDto, userDetails);
+            return ResponseEntity.ok().build();
+        } catch (CustomException e) {
+            return ResponseEntity.status(e.getHttpStatus()).build();
+        }
+    }
+
+    @GetMapping("/api/reservation") // 예약 진행
+    public String getReservations(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                  Model model) {  // 로그인 기능 구현 완료 시 UserDetail 추가
+        List<ReservationResponseDto> reservations = reservationService.getReservations(userDetails);
+        model.addAttribute("reservations", reservations);
+        return "/reservation/reservation_history";
+    }
 }
--- src/main/java/com/example/pigonair/domain/reservation/service/ReservationServiceImpl.java
+++ src/main/java/com/example/pigonair/domain/reservation/service/ReservationServiceImpl.java
@@ -17,6 +17,8 @@ import com.example.pigonair.domain.reservation.entity.Reservation;
 import com.example.pigonair.domain.reservation.repository.ReservationRepository;
 import com.example.pigonair.domain.seat.entity.Seat;
 import com.example.pigonair.domain.seat.repository.SeatRepository;
+import com.example.pigonair.global.config.common.exception.CustomException;
+import com.example.pigonair.global.config.common.exception.ErrorCode;
 import com.example.pigonair.global.config.security.UserDetailsImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -24,10 +26,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
-	private final ReservationRepository reservationRepository;
-	private final FlightRepository flightRepository;
-	private final MemberRepository memberRepository;
-	private final SeatRepository seatRepository;
+
+    private final ReservationRepository reservationRepository;
+    private final FlightRepository flightRepository;
+    private final MemberRepository memberRepository;
+    private final SeatRepository seatRepository;
 
 	@Override
 	@Transactional
@@ -35,90 +38,101 @@ public class ReservationServiceImpl implements ReservationService {
 
 		Member member = getMember(userDetails); // 로그인 정보 확인 및 가져오기
 
-		Seat seat = seatRepository.findById(requestDto.seatId()).orElseThrow(() ->
-			new NullPointerException());    // 좌석 정보 확인 및 가져오기
+        Seat seat = getSeat(requestDto);      // 좌석 정보 확인 및 가져오기
 
-		Flight flight = flightRepository.findById(seat.getFlight().getId()).orElseThrow(() ->
-			new NullPointerException());    // 비행기 가져오기
+        Flight flight = getFlight(seat);    // 비행기 가져오기
 
-		checkIsAvailableSeat(seat); // 예약 가능한 좌석인지 확인
+        checkIsAvailableSeat(seat); // 예약 가능한 좌석인지 확인
 
-		Reservation reservation = makeReservation(member, seat, flight);    // 예약 만들기
-		seat.seatPick();    // 좌석 예매 불가로 변경
-		reservationRepository.save(reservation);
-	}
-	@Transactional
-	public void updateReservationStatus() {
-		LocalDateTime currentTime = LocalDateTime.now();
-		LocalDateTime fifteenMinutesAgo = currentTime.minusMinutes(1);
+        Reservation reservation = makeReservation(member, seat, flight);    // 예약 만들기
+        seat.seatPick();    // 좌석 예매 불가로 변경
+        reservationRepository.save(reservation);
+    }
 
-		List<Reservation> expiredReservations = reservationRepository.findByIsPaymentFalseAndReservationDateBefore(fifteenMinutesAgo);
+    @Transactional
+    public void updateReservationStatus() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime fifteenMinutesAgo = currentTime.minusMinutes(1);
 
-		for (Reservation reservation : expiredReservations) {
-			// isPayment 업데이트
-			reservationRepository.delete(reservation);
+        List<Reservation> expiredReservations = reservationRepository.findByIsPaymentFalseAndReservationDateBefore(fifteenMinutesAgo);
 
-			// Seat의 isAvailable 업데이트
-			Seat seat = reservation.getSeat();
-			seat.setIsAvailable();
-			seatRepository.save(seat);
-		}
-	}
+        for (Reservation reservation : expiredReservations) {
+            // isPayment 업데이트
+            reservationRepository.delete(reservation);
 
-	@Override
-	public List<ReservationResponseDto> getReservations(UserDetailsImpl userDetails) {
-		// 로그인 정보 확인 및 가져오기
-		Member member = getMember(userDetails);
-		// 해당 사용자의 예약 중 결제되지 않은 예약 가져오기
-		List<Reservation> reservations = reservationRepository.findByMemberAndIsPayment(member, false);
-		// responseDto 만들기
-		List<ReservationResponseDto> reservationResponseDtos = getReservationResponseDtos(reservations);
+            // Seat의 isAvailable 업데이트
+            Seat seat = reservation.getSeat();
+            seat.setIsAvailable();
+            seatRepository.save(seat);
+        }
+    }
 
-		return reservationResponseDtos;
-	}
+    @Override
+    public List<ReservationResponseDto> getReservations(UserDetailsImpl userDetails) {
+        // 로그인 정보 확인 및 가져오기
+        Member member = getMember(userDetails);
+        // 해당 사용자의 예약 중 결제되지 않은 예약 가져오기
+        List<Reservation> reservations = reservationRepository.findByMemberAndIsPayment(member, false);
+        // responseDto 만들기
+        List<ReservationResponseDto> reservationResponseDtos = getReservationResponseDtos(reservations);
 
-	// --------------------------private method--------------------------
+        return reservationResponseDtos;
+    }
 
-	private Member getMember(UserDetailsImpl userDetails) {
-		Member member = memberRepository.findById(userDetails.getUser().getId()).orElseThrow(() ->
-			new NullPointerException());
-		return member;
-	}
+    // --------------------------private method--------------------------
 
-	private static void checkIsAvailableSeat(Seat seat) {
-		if (!seat.isAvailable()) {
-			throw new IllegalArgumentException();
-		}
-	}
+    private Member getMember(UserDetailsImpl userDetails) {
+        Member member = memberRepository.findById(userDetails.getUser().getId()).orElseThrow(() ->
+                new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+        return member;
+    }
 
-	private static Reservation makeReservation(Member member, Seat seat, Flight flight) {
-		Reservation reservation = Reservation.builder()
-			.member(member)
-			.seat(seat)
-			.flight(flight)
-			.reservationDate(LocalDateTime.now())
-			.isPayment(false)
-			.build();
-		return reservation;
-	}
+    private Seat getSeat(ReservationRequestDto requestDto) {
+        Seat seat = seatRepository.findById(requestDto.seatId()).orElseThrow(() ->
+                new CustomException(ErrorCode.NOT_FOUND_SEAT));
+        return seat;
+    }
 
-	private static List<ReservationResponseDto> getReservationResponseDtos(List<Reservation> reservations) {
-		List<ReservationResponseDto> reservationResponseDtos = new ArrayList<>();
-		reservations.stream().forEach(reservation -> {
-			Long id = reservation.getId();
-			String name = reservation.getMember().getEmail();
-			Flight flight = reservation.getFlight();
-			LocalDateTime departureDate = flight.getDepartureTime();
-			LocalDateTime departureTime = flight.getDepartureTime();
-			String origin = flight.getOrigin().getFullName();
-			String destination = flight.getDestination().getFullName();
-			Long seatNumber = reservation.getSeat().getId();
-			Long price = reservation.getSeat().getPrice();
-			String phoneNumber = reservation.getMember().getPhoneNumber();
-			reservationResponseDtos.add(new ReservationResponseDto(id, name, departureDate, departureTime,
-				origin, destination, seatNumber, price, phoneNumber));
+    private Flight getFlight(Seat seat) {
+        Flight flight = flightRepository.findById(seat.getFlight().getId()).orElseThrow(() ->
+                new CustomException(ErrorCode.NOT_FOUND_FLIGHT));
+        return flight;
+    }
 
-		});
-		return reservationResponseDtos;
-	}
+    private static void checkIsAvailableSeat(Seat seat) {
+        if (!seat.isAvailable()) {
+            throw new CustomException(ErrorCode.ALREADY_RESERVED_SEAT);
+        }
+    }
+
+    private static Reservation makeReservation(Member member, Seat seat, Flight flight) {
+        Reservation reservation = Reservation.builder()
+                .member(member)
+                .seat(seat)
+                .flight(flight)
+                .reservationDate(LocalDateTime.now())
+                .isPayment(false)
+                .build();
+        return reservation;
+    }
+
+    private static List<ReservationResponseDto> getReservationResponseDtos(List<Reservation> reservations) {
+        List<ReservationResponseDto> reservationResponseDtos = new ArrayList<>();
+        reservations.stream().forEach(reservation -> {
+            Long id = reservation.getId();
+            String name = reservation.getMember().getEmail();
+            Flight flight = reservation.getFlight();
+            LocalDateTime departureDate = flight.getDepartureTime();
+            LocalDateTime departureTime = flight.getDepartureTime();
+            String origin = flight.getOrigin().getFullName();
+            String destination = flight.getDestination().getFullName();
+            Long seatNumber = reservation.getSeat().getId();
+            Long price = reservation.getSeat().getPrice();
+            String phoneNumber = reservation.getMember().getPhoneNumber();
+            reservationResponseDtos.add(new ReservationResponseDto(id, name, departureDate, departureTime,
+                    origin, destination, seatNumber, price, phoneNumber));
+
+        });
+        return reservationResponseDtos;
+    }
 }
